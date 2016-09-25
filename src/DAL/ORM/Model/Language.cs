48ce301645a11c398a05f2a3d104038701184692
Rostislav <zymotic555@mail.ru>
2016-09-25 15:00:35 +0300

    Add property Name to Language Class

--- src/DAL/ORM/Model/Language.cs
+++ src/DAL/ORM/Model/Language.cs
@@ -5,9 +5,8 @@ namespace DAL.ORM.Model
 {
     public class Language : IEntity
     {
-        [Required]
-        [Key]
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Url { get; set; }
 
         public virtual ICollection<User> User { get; set; }
