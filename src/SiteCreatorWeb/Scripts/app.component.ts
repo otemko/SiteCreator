fcf1511eaa3557472d220da8089e2f6fc557d08c
otemko <otemko@yandex.ru>
2016-10-10 02:00:02 +0300

    fix some nugs in search

--- src/SiteCreatorWeb/Scripts/app.component.ts
+++ src/SiteCreatorWeb/Scripts/app.component.ts
@@ -17,14 +17,17 @@ import { LanguageService } from './Shared/Services/language.service'
 import { Elements } from './Shared/Models/elements.model'
 import { ElementService } from './Shared/Services/elements.service'
 
+import { SearchResult } from './Shared/Models/search.model'
+import { SearchService } from './Shared/Services/search.service'
+
 import { GlobalService } from './Shared/Services/global.service'
 
 @Component({
     selector: 'my-app',
     templateUrl: './appScripts/app.component.html',
     styleUrls: ['./appScripts/app.component.css'],
-    providers: [Account, Page, Service, AccountService, SiteService, PageService, GlobalService, CommentService,
-                Language, LanguageService, Elements, ElementService],
+    providers: [Account, Page, Service, SearchResult, AccountService, SiteService, PageService, GlobalService, CommentService,
+        Language, LanguageService, Elements, ElementService, SearchService],
 })
 export class AppComponent {
 
@@ -34,7 +37,8 @@ export class AppComponent {
         private accountService: AccountService,
         private languageService: LanguageService,
         private l: Language,
-        private route: Router) { 
+        private route: Router,
+        private searchService: SearchService) { 
             this.accountService.getAccountInfo();
             this.languageService.getLanguage();
     }
@@ -42,7 +46,8 @@ export class AppComponent {
     search()
     {
         if (this.valueSearch != "" && this.valueSearch != undefined) {
-            this.route.navigate(['/search', this.valueSearch]);
+            this.searchService.getSearchResultByTerm(this.valueSearch);
+            this.route.navigate(['/search']);
         }
     }
 
